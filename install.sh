a652e31a0e762970db561e5c18f545387f055b45
Balazs Kegl <balazs.kegl@gmail.com>
2017-09-08 22:47:42 +0200

    install

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
-attach volume as /dev/sdf
+# A script that installs the starting kit, the environment to execute it,
+# and downloads the data
 
-sudo mount /dev/xvdf1 ramp
-sudo chroot /home/ubuntu/ramp
-exit
-sudo mount --bind /dev /home/ubuntu/ramp
-sudo chroot /home/ubuntu/ramp
+wget -q http://repo.continuum.io/miniconda/Miniconda-3.6.0-Linux-x86_64.sh -O miniconda.sh
+chmod +x miniconda.sh
+./miniconda.sh -b -p /home/ubuntu/miniconda
+export PATH=/home/ubuntu/miniconda/bin:$PATH
+sudo apt-get update
+sudo apt-get install --yes build-essential
+sudo apt-get install --yes unzip
+sudo apt-get install --yes screen
+conda update --yes --quiet conda
+
+
+# Clone starting kit and download data
+git clone https://github.com/ramp-kits/pollenating_insects_3
+cd pollenating_insects_3
+python download_data.py
