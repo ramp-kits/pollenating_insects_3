a4f2009664b983262bdf7f1dc72a94472b4a89f2
Mehdi Cherti <mehdicherti@gmail.com>
2017-09-10 15:40:19 +0200

    add tensorflow submissions *_tf, which use an input shape of (h, w, 3) instead of (3, h, w).

---
+++ submissions/keras_pretrained_tf/batch_classifier.py
@@ -0,0 +1,52 @@
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers import BatchNormalization
+from keras.layers import Activation
+from keras.applications.vgg16 import VGG16
+from keras.optimizers import SGD
+from rampwf.workflows.image_classifier import get_nb_minibatches
+
+
+class BatchClassifier(object):
+    def __init__(self):
+        self.model = self._build_model()
+
+    def fit(self, gen_builder):
+        batch_size = 16
+        gen_train, gen_valid, nb_train, nb_valid =\
+            gen_builder.get_train_valid_generators(
+                batch_size=batch_size, valid_ratio=0.1)
+        self.model.fit_generator(
+            gen_train,
+            steps_per_epoch=get_nb_minibatches(nb_train, batch_size),
+            epochs=1,
+            max_queue_size=16,
+            workers=1,
+            use_multiprocessing=True,
+            validation_data=gen_valid,
+            validation_steps=get_nb_minibatches(nb_valid, batch_size),
+            verbose=1)
+
+    def predict_proba(self, X):
+        return self.model.predict(X)
+
+    def _build_model(self):
+        vgg16 = VGG16(include_top=False, weights='imagenet')
+        # vgg16.trainable = False
+        inp = Input((224, 224, 3))
+        x = vgg16(inp)
+        x = Flatten(name='flatten')(x)
+        x = Dense(4096, activation='linear', name='fc1')(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+        x = Dense(4096, activation='linear', name='fc2')(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)        
+        out = Dense(403, activation='softmax', name='predictions')(x)
+        model = Model(inp, out)
+        model.compile(
+            loss='categorical_crossentropy', optimizer=SGD(lr=1e-4),
+            metrics=['accuracy'])
+        return model
